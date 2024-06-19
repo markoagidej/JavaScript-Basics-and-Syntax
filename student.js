6803d254afc219b2a4a1343d255eb5d8ad7cbfe1
Marko <markogidej@yahoo.com>
2024-06-19 16:20:59 -0400

    Q1 all tasks done

---
+++ student.js
@@ -0,0 +1,13 @@
+// Task 1: Declare variables to represent student information such as name, age, grade, and attendance status.
+// Task 2: Assign sample values to the student information variables.
+
+let studentName = "Alice";
+let studentAge = 15;
+let studentGrade = 10;
+let isPresent = true;
+
+// Task 3: Display the student information using console.log() statements.
+console.log(studentName)
+console.log(studentAge)
+console.log(studentGrade)
+console.log(isPresent)
